fe282e3760d0b9737df4107134f8f2219116a595
Michael De La Rosa <56710845+m-delarosa@users.noreply.github.com>
2020-06-24 14:37:28 -0600

    Add main countainer, background image and overlay.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './App.css'
 import Nav from './components/containers/Nav'
+import Main from './components/containers/Main'
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Main />
     </div>
   )
 }
---
+++ src/components/containers/Main.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const Main = () => {
+    return (
+        <div class="background">
+            <div class="overlay center">
+                <h1 class="title">Trail Conditions</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Main
