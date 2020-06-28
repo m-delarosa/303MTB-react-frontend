58c12e1996788ee937e0d8727cb6ca9eb4f05736
github username <56710845+m-delarosa@users.noreply.github.com>
2020-06-28 15:26:22 -0600

    Add RegistrationModal functionality, handle server response.

--- src/components/elements/RegisterModal.js
+++ src/components/elements/RegisterModal.js
@@ -10,6 +10,64 @@ const RegisterModal = (props) => {
         toggleLoginModal()
     }
 
+    const handleRegistration = (event) => {
+        event.preventDefault()
+        const $registerForm = event.target
+        const registrationFormData = new FormData($registerForm)
+        const name = registrationFormData.get("name")
+        const username = registrationFormData.get("username")
+        const email = registrationFormData.get("email")
+        const password = registrationFormData.get("password")
+        const registrationBody = { user: { name, username, email, password } }
+
+        fetch("http://localhost:3000/users", {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(registrationBody),
+        })
+            .then((response) => response.json())
+            .then((result) => {
+                // console.log(result)
+                // console.log(result.token)
+                // localStorage.setItem("token", result.token)
+                handleRegistrationUserResponse(result, $registerForm)
+            })
+    }
+
+    const handleRegistrationUserResponse = (response, $form) => {
+        console.log("handleRegistrationResponse Fired!")
+        const $errorMessages = document.getElementById("response-error-messages")
+
+        if ($errorMessages) {
+            $form.removeChild($errorMessages)
+        }
+        if (response.user) {
+            console.log("User created")
+            $form.reset()
+            const $responseMessage = document.createElement("li")
+            $responseMessage.innerText = `Welcome ${response.user.name}, your user account was created sucessfully.\n Try logging in.`
+            $form.append($responseMessage)
+        } else {
+            const responseKeys = Object.keys(response)
+            const $errorMessages = document.createElement("ul")
+            $errorMessages.id = "response-error-message"
+            $errorMessages.innerText = "We were unable to create your account for the following reasons:"
+
+            responseKeys.forEach((responseKey) => {
+                const $li = document.createElement("li")
+                $li.id = "response-error-message"
+                $li.innerText = response[responseKey]
+                $errorMessages.append($li)
+            })
+
+            $form.prepend($errorMessages)
+            //   $responseList.append($responseMessage)
+        }
+    }
+
     return (
         <div>
             <Modal
@@ -19,7 +77,7 @@ const RegisterModal = (props) => {
                 onRequestClose={toggleRegisterModal}>
                 <div className="close" onClick={toggleRegisterModal}>+</div>
                 <FontAwesomeIcon icon="user-circle" size="8x" color="green" />
-                <form class="register-form">
+                <form className="register-form" onSubmit={handleRegistration}>
                     <input type="text" name="name" placeholder="Name" />
                     <input type="text" name="username" placeholder="Username" />
                     <input type="text" name="email" placeholder="Email" />
