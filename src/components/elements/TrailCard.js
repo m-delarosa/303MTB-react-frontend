6a1771722149540da1b64e3b40d15c93b5926cbc
github username <56710845+m-delarosa@users.noreply.github.com>
2020-07-06 02:02:11 -0600

    Add mobile trail card finished styling 1.0

--- src/components/elements/TrailCard.js
+++ src/components/elements/TrailCard.js
@@ -51,45 +51,49 @@ const TrailCard = (props) => {
     }
 
     return (
-        <article className="trails-card">
-            <section className="mobile-card-header">
-                <div id={`${trail.id}-status`} className="mobile-status-symbol">
-                    {status ? addStatusImage(status) : addStatusImage(trail.conditionStatus)}
+        <>
+            <article className="trails-card">
+                <section className="mobile-card-header">
+                    <h2 className="trail-name center">
+                        <a href="#preview" onClick={handlePreview}>{trail.name} </a>
+                    </h2>
+                </section>
+                <div className="mobile-image-container">
+                    <img
+                        className="trail-card-img center"
+                        src={trail.imgSmall}
+                        alt={props.name}
+                        onClick={handlePreview} />
+                    <div id={`${trail.id}-status`} className="mobile-status-symbol">
+                        {status ? addStatusImage(status) : addStatusImage(trail.conditionStatus)}
+                    </div>
                 </div>
-                <h2 className="trail-name center">
-                    <a href="#preview" onClick={handlePreview}>{trail.name} </a>
-                </h2>
-            </section>
-            <img
-                className="trail-card-img center"
-                src={trail.imgSmall}
-                alt={props.name}
-                onClick={handlePreview} />
-            <section className="trails-card-summary center">
-                <div id={`${props.id}-details`} className="condition-details">
-                    <p className="mobile-report">{description ? description : trail.conditionDetails}</p>
-                    <p className="mobile-report-date"><b>Last Report:</b> {addDate(trail.conditionDate)}.</p>
-                </div>
-            </section>
-            <section className="mobile-trail-actions">
-                {favoriteTrails.find(trail => trail.id === trail.id)
-                    ? <FontAwesomeIcon icon={['fas', 'heart']} color="#ff8f00" className="trail-listing-action center" onClick={handleFavorite} />
-                    : <FontAwesomeIcon icon={['far', 'heart']} color="#ff8f00" className="trail-listing-action center" onClick={handleFavorite} />}
-                <FontAwesomeIcon
-                    icon={['far', 'edit']}
-                    color="#ff8f00"
-                    className="trail-listing-action center trail-edit-icon"
-                    onClick={handleUpdate} />
-                <a
-                    href={`https://www.google.com/maps/dir/?api=1&origin=${props.userLocation.lat},${props.userLocation.long}&destination=${props.trailLat},${props.trailLong}`}
-                    rel="noopener noreferrer"
-                    target="_blank">
-                    <FontAwesomeIcon icon={['fas', 'directions']} color="#ff8f00" className="trail-listing-action center" />
-                </a>
-            </section>
-
-        </article>
+                <section className="trails-card-summary center">
+                    <div id={`${props.id}-details`} className="condition-details">
+                        <p className="mobile-report">{description ? description : trail.conditionDetails}</p>
+                        <p className="mobile-report-date"><b>Last Report:</b> {addDate(trail.conditionDate)}.</p>
+                    </div>
+                </section>
+                <section className="mobile-trail-actions">
+                    {favoriteTrails.find(trail => trail.id === trail.id)
+                        ? <FontAwesomeIcon icon={['fas', 'heart']} color="#ff8f00" className="trail-listing-action center" onClick={handleFavorite} />
+                        : <FontAwesomeIcon icon={['far', 'heart']} color="#ff8f00" className="trail-listing-action center" onClick={handleFavorite} />}
+                    <FontAwesomeIcon
+                        icon={['far', 'edit']}
+                        color="#ff8f00"
+                        className="trail-listing-action center trail-edit-icon"
+                        onClick={handleUpdate} />
+                    <a
+                        href={`https://www.google.com/maps/dir/?api=1&origin=${props.userLocation.lat},${props.userLocation.long}&destination=${props.trailLat},${props.trailLong}`}
+                        rel="noopener noreferrer"
+                        target="_blank">
+                        <FontAwesomeIcon icon={['fas', 'directions']} color="#ff8f00" className="trail-listing-action center" />
+                    </a>
+                </section>
+            </article>
+        </>
     )
 }
 
 export default TrailCard
+
