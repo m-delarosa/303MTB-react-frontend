5821815a4436dc27f88ee3e7a78eb2cd1b0c1572
github username <56710845+m-delarosa@users.noreply.github.com>
2020-10-15 21:01:44 -0600

    Remove paths and compenent imports from Main.js to meet MVP.

--- src/components/containers/Main.js
+++ src/components/containers/Main.js
@@ -1,9 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Home from './Home'
-import About from './About'
 import Trails from './Trails'
-import TrailFinder from './TrailFinder'
 
 const Main = () => {
     return (
@@ -12,9 +9,6 @@ const Main = () => {
                 <div className="overlay">
                     <Switch>
                         <Route path="/" exact component={Trails} />
-                        <Route path="/trails" component={Trails} />
-                        <Route path="/trailfinder" component={TrailFinder} />
-                        <Route path="/about" component={About} />
                     </Switch>
                 </div>
             </main>
